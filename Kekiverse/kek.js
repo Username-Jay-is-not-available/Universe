abb3706c173ebf0cc7d7f1a53f4facf7feebc060
J <102687512+Username-Jay-is-not-available@users.noreply.github.com>
2022-10-17 18:11:04 -0700

    Add files via upload
    Most updated version (6:00pm 10/17/22)

--- Kekiverse/kek.js
+++ Kekiverse/kek.js
@@ -1,10 +1,11 @@
 /*
- * Write your routing code in this file.  Make sure to add your name and
- * @oregonstate.edu email address below.
- *
  * Name: Jaaron Green
  * Email: greenjaa@oregonstate.edu
  */
+
+
+//NOTES:
+//Might have to change 'index' to kekiverse
 
 var path = require('path');
 var express = require('express');
@@ -21,17 +22,38 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'));
 
 
-app.get('/', function (req, res) { //might have to do :author or something for singele twit
+//Add function that retrieves text from the search bar on click 
+
+
+
+
+//DOES NOT WORK. SEE WHAT TO DO
+
+ //Gets search bar information
+ var SB = document.querySelector('#navbar-search-button') //Document does not work. Need separate file?
+ var search = document.getElementById('navbar-search-input').value
+ 
+ 
+ 
+ //Change this function to assign a number to the search bar
+ SB.addEventListener('click', function (event) {
+
+  var SBTEXT = document.getElementById('navbar-search-input').value
+  search = SBTEXT
+
+})
+
+app.get('/', function (req, res) { 
   if(kekData) {
-    res.status(200).render('index', { //Might have to change index to kek
-      kekData,
-      RenderShit: true
+    res.status(200).render('kekiverse', { 
+      RenderShit: true,
+      kekData: [kekData[0]] //This is the home page
     }) 
   }
 
 })
 
-app.get('/:noun', function (req, res, next) {
+app.get('/:noun', function (req, res, next) { //0 is default
 
     //Edit to get the name of the search instead of the index number
 
@@ -40,9 +62,8 @@ app.get('/:noun', function (req, res, next) {
   console.log("====Shit: ", TheShit)
 
   if(TheShit) {
-    res.status(200).render('index', {
+    res.status(200).render('kekiverse', {
       RenderShit: true,
-      //SingleTwit: true,
       kekData: [kekData[musk]]
     })
   }
@@ -55,7 +76,7 @@ app.get('/:noun', function (req, res, next) {
 
 
 app.get('*', function (req, res) {
-  res.status(404).render('index', {
+  res.status(404).render('kekiverse', {
     RenderShit: false
   })
 });
