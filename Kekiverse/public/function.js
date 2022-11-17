60fdb3ef54633b842ec17aecf2469c45fc078126
J <102687512+Username-Jay-is-not-available@users.noreply.github.com>
2022-11-16 17:51:35 -0800

    Add files via upload

---
+++ Kekiverse/public/function.js
@@ -0,0 +1,42 @@
+function sendText() {
+    // Get the value of the text in search bar
+
+    let searchInput = document.getElementById("navbar-search-input").value;
+
+    let dict = {
+        "query": searchInput
+    }
+
+    // Setup AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://localhost:3000/search/?q=" + searchInput, true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+
+    // Tell AJAX request how to resolve 
+
+    xhttp.onreadystatechange = () => {
+        if (xhttp.status == 200) {
+
+            window.location.assign(xhttp.responseURL);
+        }
+        else if (xhttp.status == 404) {
+            window.location.assign("http://localhost:5000/%22*%22");
+            console.log("No matching record found")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(dict));
+
+}
+
+
+function ClickYou() {
+
+    var searchButton = document.getElementById('navbar-search-button');
+
+    if (searchButton) {
+        searchButton.addEventListener('click', sendText);
+    }
+
+}
